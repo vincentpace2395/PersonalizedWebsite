18cb6175ec9964c7766c659a87a3125f8cc93c65
Vincent Pace <vpace@nhpace.net>
2019-07-12 20:50:33 -0500

    Add primary components, react-router with routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from './Header/Header';
+import history from './history';
+import HomePage from '../components/Home/Home';
+import Resume from '../components/Resume/Resume';
+import Profiles from '../components/Profiles/Profiles';
+import About from '../components/About/About';
+import {Router, Route, Switch} from 'react-router-dom';
 
 const App = () => {
-   return (
-        <div>
-            <Header />
+    return (
+        <div className='ui container '>
+            <Router history={history}>
+                <div>
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/resume" exact component={Resume} />
+                        <Route path="/profiles" exact component={Profiles} />
+                        <Route path="/about" exact component={About} />
+                    </Switch>
+                </div>
+            </Router>
         </div>
-    )
+    );
 };
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import About from '../About/About';
+import Profiles from '../Profiles/Profiles';
+import Resume from '../Resume/Resume'
+import history from '../history';
 import './Header.css';
+import { Router, Link } from 'react-router-dom';
 
 class Header extends React.Component {
+    state = {
+        render: null
+    };
+
+     onResumeTabClicked = () => {
+        return (
+            this.setState({
+                render: Resume
+            })
+        );
+    };
+
+    onProfilesTabClicked = () => {
+        return (
+            this.setState({
+                render: Profiles
+            })
+        );
+    };
+
+    onAboutTabClicked = () => {
+        return (
+            this.setState({
+                render: About
+            })
+        );
+    };
+
     render() {
         return (
             <Navbar bg="light" expand="lg">
@@ -11,9 +44,9 @@ class Header extends React.Component {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className='justify-content-end' id="basic-navbar-nav">
                     <Nav>
-                        <Nav.Link href="resume">Resume</Nav.Link>
-                        <Nav.Link href="profiles">Profiles</Nav.Link>
-                        <Nav.Link href="about">About Me</Nav.Link>
+                        <Nav.Link onClick={this.onResumeTabClicked}>Resume</Nav.Link>
+                        <Nav.Link onClick={this.onProfilesTabClicked}>Profiles</Nav.Link>
+                        <Nav.Link onClick={this.onAboutTabClicked}>About Me</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
