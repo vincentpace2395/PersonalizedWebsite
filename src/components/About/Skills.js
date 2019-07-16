fb2b812753ef569d506f11a2a9ec5b6f5f70cc4f
Vincent Pace <vpace@nhpace.net>
2019-07-16 00:39:30 -0500

    Add images for skills to Skills.js component

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -2,15 +2,79 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div style={{ marginTop: '40px', paddingLeft: '75px' }}>
-            <h6>JavaScript</h6>
-            <h6>ReactJS</h6>
-            <h6>Redux</h6>
-            <h6>Java</h6>
-            <h6>Selenium WebDriver</h6>
-            <h6>PostgreSQL</h6>
-            <h6>Agile</h6>
-            <h6>Firebase</h6>
+        <div style={{ marginTop: '40px', width: '60%' }}>
+            <img
+                alt='javascript-avatar'
+                className='avatar-img'
+                src="https://d2vqpl3tx84ay5.cloudfront.net/500x/tumblr_lsus01g1ik1qies3uo1_400.png"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='react-avatar'
+                className='avatar-img'
+                src="http://www.technoscore.com/images/services/react-js-icon.png"
+                style={{margin: '8px', height: '75px', width: '75px', backgroundColor: 'grey'}}
+            />
+            <img
+                alt='redux-avatar'
+                className='avatar-img'
+                src="https://www.valentinog.com/blog/wp-content/uploads/2017/12/redux-logo.png"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+             />
+            <img
+                alt='github-avatar'
+                className='avatar-img'
+                src="https://i.pinimg.com/736x/5b/59/f4/5b59f4c5a4df1f00a482a11a3d4880f8--github-logo-labs.jpg"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='java-avatar'
+                className='avatar-img'
+                src="https://pngimage.net/wp-content/uploads/2018/06/java-logo-png-transparent-background-7.png"
+                style={{margin: '8px', height: '75px', width: '75px', backgroundColor: 'white'}}
+            />
+            <img
+                alt='html-avatar'
+                className='avatar-img'
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='selenium-avatar'
+                className='avatar-img'
+                src="https://www.3pillarglobal.com/wp-content/uploads/2015/11/xselenium_logo_320x260-300x260.jpg.pagespeed.ic.aqA1rifk_N.jpg"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='postgres-avatar'
+                className='avatar-img'
+                src="https://banner2.kisspng.com/20180421/lkw/kisspng-postgresql-database-logo-computer-icons-replicatio-software-developer-5adbf4c2ceb373.0739840815243644828467.jpg"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='docker-avatar'
+                className='avatar-img'
+                src="https://i2.wp.com/blog.docker.com/wp-content/uploads/2013/06/Docker-logo-011.png?ssl=1"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='firebase-avatar'
+                className='avatar-img'
+                src="https://pbs.twimg.com/profile_images/733010550670557184/aMmM_m3g.jpg"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
+            <img
+                alt='jenkins-avatar'
+                className='avatar-img'
+                src="https://dwglogo.com/wp-content/uploads/2017/11/1500px_Jenkins_logo.png"
+                style={{margin: '8px', height: '75px', width: '75px', backgroundColor: 'lightgrey'}}
+            />
+            <img
+                alt='agile-avatar'
+                className='avatar-img'
+                src="https://i2.wp.com/s3.amazonaws.com/production-wordpress-assets/blog/wp-content/uploads/2017/01/16102723/agile.png?fit=441%2C270&ssl=1"
+                style={{margin: '8px', height: '75px', width: '75px'}}
+            />
         </div>
     );
 };
