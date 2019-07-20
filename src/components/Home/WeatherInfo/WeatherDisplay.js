46f45f066b980444534f957d74c12e99750c196d
Vincent Pace <vpace@nhpace.net>
2019-07-20 18:53:21 -0500

    Begin working on axios request to fetch weather data

--- src/components/Home/WeatherInfo/WeatherDisplay.js
+++ src/components/Home/WeatherInfo/WeatherDisplay.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 class WeatherDisplay extends React.Component {
-
+    state = {
+        temperature: null,
+    };
 
     componentDidMount() {
-
+        axios.get('http://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=7b4435273deac153aa81d70948d836c9')
+            .then(response => {
+                this.setState({
+                    temperature: response
+                });
+            }
+        )
     }
 
     render() {
         return (
             <div>
                 <div style={{width: '30%', paddingTop: '200px'}}>
-                    WeatherDisplay
+                    {this.state.temperature}
                 </div>
                 <div style={{paddingTop: '25px'}}>
 
