12505221e83cce5fe809cbf347b08e2ba4a8e24a
Vincent Pace <vpace@nhpace.net>
2019-07-19 23:09:12 -0500

    Add Weather components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Bio from '../Bio/Bio';
 import Timeline from './Timeline/Time';
 import Intro from './Intro/Intro';
+import WeatherOption from "./WeatherInfo/WeatherOption";
+import WeatherDisplay from "./WeatherInfo/WeatherDisplay";
 
 const Home = () => {
     return (
         <div>
             <Bio category = <Intro /> />
             <Timeline />
+            <WeatherDisplay />
+            <div>
+                <WeatherOption />
+            </div>
         </div>
     );
 };
---
+++ src/components/Home/WeatherInfo/WeatherDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class WeatherDisplay extends React.Component {
+    render() {
+        return (
+            <div style={{width: '30%', paddingTop: '200px'}}>
+                WeatherDisplay
+            </div>
+        );
+    }
+};
+
+export default WeatherDisplay;
