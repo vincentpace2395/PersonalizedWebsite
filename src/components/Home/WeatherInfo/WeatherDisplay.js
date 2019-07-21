87722f15336c40948b54f6172cd9d3fb2e5ede3a
Vincent Pace <vpace@nhpace.net>
2019-07-21 01:12:39 -0500

    Add error handling, Fragment around WeatherDisplay component

--- src/components/Home/WeatherInfo/WeatherDisplay.js
+++ src/components/Home/WeatherInfo/WeatherDisplay.js
@@ -4,29 +4,43 @@ import axios from 'axios';
 class WeatherDisplay extends React.Component {
     state = {
         temperature: null,
+        error: undefined
+    };
+
+    kelvinToFahrenheit = kelvin => {
+        return Math.floor((kelvin - 273.15) * (9/5) + 32);
     };
 
     componentDidMount() {
-        axios.get('http://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=7b4435273deac153aa81d70948d836c9')
+        axios.get('http://api2.openweathermap.org/data/2.5/weather?zip=78758,us&APPID=7b4435273deac153aa81d70948d836c9')
             .then(response => {
                 this.setState({
-                    temperature: response
+                    temperature: this.kelvinToFahrenheit(response.data.main.temp)
                 });
-            }
-        )
+            })
+            .catch(error => {
+                this.setState({
+                    error: error.message
+                })
+            });
     }
 
     render() {
         return (
-            <div>
-                <div style={{width: '30%', paddingTop: '200px'}}>
-                    {this.state.temperature}
-                </div>
-                <div style={{paddingTop: '25px'}}>
+            <React.Fragment>
+                {!this.state.error ? (
+                    <div>
+                        <div style={{width: '30%', paddingTop: '200px'}}>
+                            {this.state.temperature}
+                        </div>
+                        <div style={{paddingTop: '25px'}}>
 
-                </div>
-            </div>
-
+                        </div>
+                    </div>
+                ) : (
+                    <div style={{color: 'red', width: '30%', paddingTop: '200px'}}>{this.state.error}</div>
+                )}
+            </React.Fragment>
         );
     }
 };
