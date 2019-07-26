e1e20036164e7d82508d9f26804d77fb8562a268
Vincent Pace <vpace@nhpace.net>
2019-07-25 19:48:27 -0500

    Add title attributes to images to display tooltip

--- src/components/About/ContactInfo/ContactOptions/ContactOptions.js
+++ src/components/About/ContactInfo/ContactOptions/ContactOptions.js
@@ -1,42 +1,28 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 import './ContactOptions.css';
 
 class ContactOptions extends React.Component {
-    displayEmail = () => {
-        console.log('vpace@nhpace.net');
-        return <div>vpace@nhpace.net</div>
-    };
-
-    displayPhoneNumber = () => {
-        console.log('(603) 714-1382');
-        return <div style={{display: 'block', fontSize: '100px'}}>(603) 714-1382</div>
-    };
-
-    displayLinkedinUrl = () => {
-        console.log('https://www.linkedin.com/in/vincent-pace-394562b8/');
-        return <div>https://www.linkedin.com/in/vincent-pace-394562b8/</div>
-    };
-
     render() {
         return (
             <div style={{width: '30%', float: 'right', paddingLeft: '4%', paddingBottom: '20px'}}>
                 <img
+                    title="vpace@nhpace.net"
                     className="gmail-icon-link"
                     src="https://www.contactmonkey.com/cm_wp/wp-content/uploads/2019/04/gmail-3.png"
                     style={{margin: '5px', width: '60px', height: '60px', backgroundColor: 'black'}}
-                    onMouseOver={this.displayEmail}
                 />
                 <img
+                    title="(603) 714-1382"
                     className="phone-icon-link"
                     src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png"
                     style={{margin: '5px', width: '60px', height: '60px', backgroundColor: 'white'}}
-                    onMouseOver={this.displayPhoneNumber}
                 />
                 <img
+                    title="https://www.linkedin.com/in/vincent-pace-394562b8/"
                     className="linkedin-icon-link"
                     src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"
                     style={{margin: '5px', width: '60px', height: '60px', backgroundColor: 'white'}}
-                    onMouseOver={this.displayLinkedinUrl}
                 />
             </div>
         );
--- src/components/About/Skills/Skills.js
+++ src/components/About/Skills/Skills.js
@@ -5,72 +5,84 @@ const Skills = () => {
         <div style={{ marginTop: '40px', width: '60%' }}>
             <img
                 alt='javascript-avatar'
+                title="JavaScript"
                 className='avatar-img'
                 src="https://d2vqpl3tx84ay5.cloudfront.net/500x/tumblr_lsus01g1ik1qies3uo1_400.png"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='react-avatar'
+                title="React"
                 className='avatar-img'
                 src="http://www.technoscore.com/images/services/react-js-icon.png"
                 style={{margin: '8px', height: '75px', width: '75px', backgroundColor: 'grey'}}
             />
             <img
                 alt='redux-avatar'
+                title="Redux"
                 className='avatar-img'
                 src="https://www.valentinog.com/blog/wp-content/uploads/2017/12/redux-logo.png"
                 style={{margin: '8px', height: '75px', width: '75px'}}
              />
             <img
                 alt='github-avatar'
+                title="GitHub"
                 className='avatar-img'
                 src="https://i.pinimg.com/736x/5b/59/f4/5b59f4c5a4df1f00a482a11a3d4880f8--github-logo-labs.jpg"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='java-avatar'
+                title="Java"
                 className='avatar-img'
                 src="https://pngimage.net/wp-content/uploads/2018/06/java-logo-png-transparent-background-7.png"
                 style={{margin: '8px', height: '75px', width: '75px', backgroundColor: 'white'}}
             />
             <img
                 alt='html-avatar'
+                title="HTML"
                 className='avatar-img'
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='selenium-avatar'
+                title="Selenium WebDriver"
                 className='avatar-img'
                 src="https://www.3pillarglobal.com/wp-content/uploads/2015/11/xselenium_logo_320x260-300x260.jpg.pagespeed.ic.aqA1rifk_N.jpg"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='postgres-avatar'
+                title="PostgreSQL"
                 className='avatar-img'
                 src="https://banner2.kisspng.com/20180421/lkw/kisspng-postgresql-database-logo-computer-icons-replicatio-software-developer-5adbf4c2ceb373.0739840815243644828467.jpg"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='docker-avatar'
+                title="Docker"
                 className='avatar-img'
                 src="https://i2.wp.com/blog.docker.com/wp-content/uploads/2013/06/Docker-logo-011.png?ssl=1"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='firebase-avatar'
+                title="Firebase"
                 className='avatar-img'
                 src="https://pbs.twimg.com/profile_images/733010550670557184/aMmM_m3g.jpg"
                 style={{margin: '8px', height: '75px', width: '75px'}}
             />
             <img
                 alt='jenkins-avatar'
+                title="Jenkins"
                 className='avatar-img'
                 src="https://dwglogo.com/wp-content/uploads/2017/11/1500px_Jenkins_logo.png"
                 style={{margin: '8px', height: '75px', width: '75px', backgroundColor: 'lightgrey'}}
             />
             <img
                 alt='agile-avatar'
+                title="Agile"
                 className='avatar-img'
                 src="https://i2.wp.com/s3.amazonaws.com/production-wordpress-assets/blog/wp-content/uploads/2017/01/16102723/agile.png?fit=441%2C270&ssl=1"
                 style={{margin: '8px', height: '75px', width: '75px'}}
